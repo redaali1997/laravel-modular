20d42faf4e865289156395d4010c2a2433e955d3
Reda Ali <reda5201028@gmail.com>
2024-01-28 06:52:55 +0200

    Automated tests (not completed)

---
+++ modules/Order/Tests/OrderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Order\Tests;
+
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    public function it_creates_an_order()
+    {
+        dd('here');
+        $this->assertTrue(true);
+    }
+}
